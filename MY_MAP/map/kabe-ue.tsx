9c782bc191cda5424ac623fa3a35deb2fa7e8517
hj-g18005 <hj-g18005@sist.ac.jp>
2020-01-24 15:06:43 +0900

    マップ変更開始

---
+++ MY_MAP/map/kabe-ue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="kabe-ue" tilewidth="40" tileheight="40" tilecount="300" columns="20">
+ <image source="../../元素材/mapchip/kabe-ue.png" width="800" height="600"/>
+</tileset>
---
+++ MY_MAP/map/pipo-map001_at-sabaku.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="pipo-map001_at-sabaku" tilewidth="40" tileheight="40" tilecount="5" columns="1">
+ <image source="../../../../../../素材/マップチップ/pipo-map001/800x600/pipo-map001_at-sabaku.png" width="40" height="200"/>
+</tileset>
