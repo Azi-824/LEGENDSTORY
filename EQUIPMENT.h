713810e85310363d117e2b0b7ff4ac70c2b22091
hj-g18005 <hj-g18005@sist.ac.jp>
2020-03-05 21:41:07 +0900

    装備クラス追加中断 リストクラス変更開始

---
+++ EQUIPMENT.h
@@ -0,0 +1,45 @@
+//EQUIPMENT.hpp
+//装備関係の元になるクラス
+
+#pragma once
+
+//##################### ヘッダファイル読み込み #######################
+#include "DxLib.h"
+#include <vector>
+#include <string>
+
+//##################### マクロ定義 #########################
+
+//##################### クラス定義 #########################
+class EQUIPMENT
+{
+private:
+
+	std::vector<std::string> Name;			//装備名
+	std::vector<int> EquipNum;				//装備番号
+	std::vector<int> PossessionNum;			//装備の所持数
+	std::vector<bool> EquipFlg;				//装備しているかどうか
+	std::vector<std::string> Description;	//説明文
+
+public:
+
+	EQUIPMENT();							//コンストラクタ
+	~EQUIPMENT();							//デストラクタ
+
+	//セッタ
+	void SetName(int,const char *);			//装備名設定
+	void SetEquipNum(int,int);				//装備番号設定
+	void SetPossessionNum(int,int);			//装備所持数設定
+	void SetEquipFlg(int, bool);			//装備しているかどうか設定
+	void SetDescription(int, const char *);	//説明文設定
+
+	//ゲッタ
+	const char * GetName(int);				//装備名取得
+	int GetEquipNum(int);					//装備番号取得
+	int GetPossessionNum(int);				//装備所持数取得
+	bool GetEquipFlg(int);					//装備しているか取得
+	const char * GetDescription(int);		//説明文取得
+
+	//bool Add()
+
+};
