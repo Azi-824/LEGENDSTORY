9d0d20807c63a91e866eb2926321831035bd8f67
hj-g18005 <hj-g18005@sist.ac.jp>
2020-03-06 15:07:03 +0900

    装備クラス修正

--- EQUIPMENT.cpp
+++ EQUIPMENT.cpp
@@ -7,4 +7,68 @@
 //####################### クラス定義 ##########################
 
 //コンストラクタ
+EQUIPMENT::EQUIPMENT()
+{
+	return;
+}
 
+//デストラクタ
+EQUIPMENT::~EQUIPMENT()
+{
+	//vectorのメモリ解放を行う
+	std::vector<int> v;			//空のvectorを作成する
+	this->CodeNum.swap(v);		//空と中身を入れ替える
+
+	//vectorのメモリ解放を行う
+	std::vector<int> v2;			//空のvectorを作成する
+	this->PossessionNum.swap(v2);	//空と中身を入れ替える
+
+	//vectorのメモリ解放を行う
+	std::vector<bool> v3;			//空のvectorを作成する
+	this->EquipFlg.swap(v3);		//空と中身を入れ替える
+
+	return;
+
+}
+
+//装備所持数設定
+void EQUIPMENT::SetPossessionNum(int kind, int possesionnum)
+{
+	this->PossessionNum[kind] = possesionnum;	//所持数設定
+	return;
+}
+
+//装備フラグ設定
+void EQUIPMENT::SetEquipFlg(int kind, bool equipflg)
+{
+	this->EquipFlg[kind] = equipflg;	//装備フラグ設定
+	return;
+}
+
+//コード番号取得
+int EQUIPMENT::GetCodeNum(int kind)
+{
+	return this->CodeNum[kind];
+}
+
+//装備所持数取得
+int EQUIPMENT::GetPossessionNum(int kind)
+{
+	return this->PossessionNum[kind];
+}
+
+//装備フラグ取得
+bool EQUIPMENT::GetEquipFlg(int kind)
+{
+	return this->EquipFlg[kind];
+}
+
+//装備追加
+void EQUIPMENT::Add(int codenum)
+{
+	this->CodeNum.push_back(codenum);		//コード番号追加
+	this->PossessionNum.push_back(1);		//装備所持数を追加
+	this->EquipFlg.push_back(false);		//追加した装備の装備状態を設定
+	
+	return;
+}
--- EQUIPMENT.h
+++ EQUIPMENT.h
@@ -15,11 +15,11 @@ class EQUIPMENT
 {
 private:
 
-	std::vector<std::string> Name;			//装備名
-	std::vector<int> EquipNum;				//装備番号
+	//std::vector<std::string> Name;			//装備名
+	std::vector<int> CodeNum;				//コード番号
 	std::vector<int> PossessionNum;			//装備の所持数
 	std::vector<bool> EquipFlg;				//装備しているかどうか
-	std::vector<std::string> Description;	//説明文
+	//std::vector<std::string> Description;	//説明文
 
 public:
 
@@ -27,19 +27,19 @@ public:
 	~EQUIPMENT();							//デストラクタ
 
 	//セッタ
-	void SetName(int,const char *);			//装備名設定
-	void SetEquipNum(int,int);				//装備番号設定
+	//void SetName(int,const char *);			//装備名設定
+	//void SetCodeNum(int,int);				//コード番号設定
 	void SetPossessionNum(int,int);			//装備所持数設定
 	void SetEquipFlg(int, bool);			//装備しているかどうか設定
-	void SetDescription(int, const char *);	//説明文設定
+	//void SetDescription(int, const char *);	//説明文設定
 
 	//ゲッタ
-	const char * GetName(int);				//装備名取得
-	int GetEquipNum(int);					//装備番号取得
+	//const char * GetName(int);				//装備名取得
+	int GetCodeNum(int);					//コード番号取得
 	int GetPossessionNum(int);				//装備所持数取得
 	bool GetEquipFlg(int);					//装備しているか取得
-	const char * GetDescription(int);		//説明文取得
+	//const char * GetDescription(int);		//説明文取得
 
-	//bool Add()
+	void Add(int);							//装備情報追加
 
 };
